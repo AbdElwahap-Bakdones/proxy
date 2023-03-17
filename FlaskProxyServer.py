d236960c41d78ca5d7797b56513a7669664f9048
AbdElwahap-Bakdones <114876536+AbdElwahap-Bakdones@users.noreply.github.com>
2023-03-17 18:05:00 +0300

    Update FlaskProxyServer.py

--- FlaskProxyServer.py
+++ FlaskProxyServer.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 # import requests
 
 app = Flask(__name__)
-# app.config['SERVER_NAME'] = 'proxyMessage.com'
+app.config['SERVER_NAME'] = 'proxyMessage.com'
 
 
 @app.route('/', methods=['POST', 'GET'])
